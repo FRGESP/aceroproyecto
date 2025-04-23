3bb09852c217627cf14b2857d990e1a06058a78c
FRGESP <f.gordilloespinoza@ugto.mx>
2025-04-22 22:46:45 -0600

    Parte Front Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-
+import LoginForm from "@/components/loginForm"
 function Loginpage() {
   return (
-    <div>Loginpage</div>
+    <div className='flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-300 via-white'>
+      <div className='w-44 h-auto mb-7'>
+        <img src="/assets/login/logo.png" alt="logo"  />
+      </div>
+        <LoginForm />
+    </div>
   )
 }
 
---
+++ src/components/loginForm.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+function LoginForm() {
+  return (
+    <div>
+        <form className='flex flex-col  w-lg gap-4' onSubmit={() => console.log('submit')}>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="user" className="font-semibold  text-2xl flex-grow text-left">Usuario</label>
+            <input className='border border-black rounded-xl w-full py-3 px-3 text-xl' type="text" placeholder='Ingrese su número de usuario'/>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="user" className="font-semibold text-2xl flex-grow text-left">Contraseña</label>
+            <input className='border border-black rounded-xl w-full py-3 px-3 text-xl' type="text" placeholder='Ingrese su contraseña'/>
+          </div>
+          <button className='py-3 font-semibold text-xl text-white bg-acento hover:bg-acentohover hover:cursor-pointer rounded-3xl'>Ingresar</button>
+        </form>
+      </div>
+  )
+}
+
+export default LoginForm
